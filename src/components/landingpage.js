1b405a1eb29acb6dcd6934fdd4f0c950c206f04f
joshua jackson <jjackson17mv@gmail.com>
2019-08-15 14:29:32 -0700

    working on projects

---
+++ src/components/landingpage.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
+
+
+
+
+
+class LandingPage extends Component{
+    render(){
+        return(
+            <div style = {{width:'100%', margin:'auto'}}>
+                <Grid className ="landing-grid">
+                    <Cell col={12}>
+                        
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+                             <hr/>
+                             <p>HTML | CSS | Javascript | jQuery | Firebase | MongoDB | MySQL | Node.js | MERN stack | React</p>
+
+                             <div className="social-links">
+
+                                 <a href="https://www.linkedin.com/in/joshua-jackson-387b6593/" rel="noopener noreferrer" target="_blank">
+                                 <i class="fab fa-linkedin" aria-hidden="true"/>
+                                 </a>
+
+                                 <a href="https://github.com/jjackson17mv" rel="noopener noreferrer" target="_blank">
+                                 <i class="fab fa-github-square" aria-hidden="true" />
+                                 </a>
+
+                                
+                        </div>
+                        </div>
+                    </Cell>
+                </Grid>
+
+            </div>
+        )
+    }
+}
+
+export default LandingPage;
